5e77906f5055da584d5f6e4f3cf7a6b8cd61919c
James Gentes <james@thesocialbusiness.com>
2014-08-28 23:07:09 +0000

    removed app/app

--- app/app/shufflejs/src/intro.js
+++
@@ -1,9 +0,0 @@
-(function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['jquery', 'modernizr'], factory);
-  } else {
-    factory(window.$, window.Modernizr);
-  }
-})(function($, Modernizr, undefined) {
-
-'use strict';
