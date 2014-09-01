dca099447335f296df8981a53fcf812f2d1ebfce
James Gentes <james@thesocialbusiness.com>
2014-09-01 21:54:16 +0000

    fix for heroku

--- index.js
+++ index.js
@@ -181,8 +181,7 @@ app.use(passport.session());
 
 
 //===============ROUTES=================
-console.log(__dirname);
-console.dir(__dirname);
+
 // load the single view file (angular will handle the page changes on the front-end)
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname + config.path }); 
