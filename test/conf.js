40aed8dc25faa08ce872ceb7a4ac4bb43f16b6b3
James Gentes <james@thesocialbusiness.com>
2014-11-18 22:17:06 +0000

    updated port

--- test/conf.js
+++ test/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://' + process.env.IP + ':' + process.env.PORT + '/wd/hub',
   specs: ['todo-spec.js']
 };
