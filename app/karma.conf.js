5c61eea2b01dba59856f1023d35bd051f4126f14
James Gentes <james@thesocialbusiness.com>
2014-11-18 17:23:51 +0000

    updated karma port

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
     exclude: [],
 
     // web server port
-    port: process.env.PORT,
+    //port: process.env.PORT,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
