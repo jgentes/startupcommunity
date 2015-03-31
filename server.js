3ade2586400dc87fb3b1adbab3e1666df73e9794
James Gentes <james@startupcommunity.org>
2015-03-31 10:26:20 -0700

    moved whisper to top

--- server.js
+++ server.js
@@ -19,18 +19,18 @@ var express = require('express'),
 
 var app = express();
 
+// Restrict access to dev.startupcommunity.org
+if (process.env.NODE_ENV === "test") {
+  var wwwhisper = require('connect-wwwhisper');
+  app.use(wwwhisper());
+}
+
 // Order really matters here..!
 app.disable('x-powered-by');
 app.use(logger('dev'));
 app.use(methodOverride());
+
 // Some things must come before Body Parser
-
-// Restrict access to dev.startupcommunity.org
-if (process.env.NODE_ENV === "test") {
-    var wwwhisper = require('connect-wwwhisper');
-    app.use(wwwhisper());
-}
-
 // Proxy for Ghost, which runs on different port
 app.all("/blog*", function(req, res){
     blogProxy.web(req, res, { target: 'http://localhost:2368' });
