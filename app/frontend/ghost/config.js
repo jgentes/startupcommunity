d0e436ea9e63e403aa697fc1052a25324cc1069b
James Gentes <james@startupcommunity.org>
2015-03-31 17:54:28 -0700

    update to ghost dev

--- app/frontend/ghost/config.js
+++ app/frontend/ghost/config.js
@@ -52,7 +52,7 @@ config = {
         database: {
             client: 'sqlite3',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
             },
             debug: false
         },
