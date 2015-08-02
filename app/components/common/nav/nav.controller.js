7f1766722b8d86c1506e27e3d2e5592a7210faf5
James Gentes <james@startupcommunity.org>
2015-08-02 10:37:54 -0700

    got change community bubble working

--- app/components/common/nav/nav.controller.js
+++ app/components/common/nav/nav.controller.js
@@ -3,10 +3,8 @@ angular
     .controller('NavigationController', NavigationController);
 
 function NavigationController($scope, $state, $location, $modal, $stateParams, user, community, communities) {
-    // reference 'this' by using 'nav' from 'NavigationController as nav' - * nav is also usable in child views *
-    window.$scope = $scope; // for console testing to avoid $scope = $('body').scope()
 
-    this.user = user.data;
+    this.user = user.data; // reference 'this' by using 'nav' from 'NavigationController as nav' - * nav is also usable in child views *
 
     // sort communities for use in nav and child dashboard pages
     for (item in communities.data) {
@@ -51,32 +49,23 @@ function NavigationController($scope, $state, $location, $modal, $stateParams, u
         this.maploc = this.community.profile.name;
     } else this.maploc = this.community.profile.home;
 
-    this.setCenter = function(center) {
+    this.setMap = function(center) {
         this.hideChange = false;
-        this.latlng = {
-            "lat" : center.A,
-            "lng" : center.F
-        };
+        if (!this.latlng) {
+            this.latlng = {
+                "lat": center.A,
+                "lng": center.F
+            };
+        }
     };
 
-    if (!$scope.global) {
-        $scope.global = {
-            alert: {},
-            search: undefined
-        };
-    }
-
-    $scope.$on('mapInitialized', function(event, map) {
-        $scope.global.mapCenter = "Bend, OR";
-    });
-
-    $scope.changeLocation = function() {
+    $('body').on('click', '#changeLocation', function() {
         var modalInstance = $modal.open({
             templateUrl: 'components/common/nav/nav.change_location.html',
             controller: ChangeLocationController,
             windowClass: "hmodal-warning"
         });
-    };
+    });
 
     this.path = $location.path(); //also used in view
     if (this.path.split('/').length < 3) { //only for root routes
