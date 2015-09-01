a945f744c52db05e765fa0b0a97eef30693a9df6
James Gentes <james@thesocialbusiness.com>
2015-08-31 21:26:12 -0700

    not sure

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", express.static(__dirname + config.path));
 app.use("/public", express.static(__dirname + '/public'));
-
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   // production-only things go here
   app.use(enforce.HTTPS(true));
