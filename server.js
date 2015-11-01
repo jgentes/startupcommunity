77bdd264a93ab99fa76b701eb8d10cb4bceeeb95
James Gentes <james@startupcommunity.org>
2015-11-01 09:24:10 -0800

    added 2.0 api set back in for migration

--- server.js
+++ server.js
@@ -87,6 +87,27 @@ var AuthApi = require('./api/auth.api.js'),
     maint = new MaintApi();
 
 // API
+
+// LEGACY 2.0 (REMOVE AFTER JAN 1, 2016)
+app.get('/api/2.0/key/:key', communityApis.getKey);
+app.get('/api/2.0/users', userApis.userSearch);
+app.get('/api/2.0/search', userApis.directSearch);
+app.get('/api/2.0/community', communityApis.getCommunity);
+app.get('/api/2.0/community/:community', communityApis.getCommunity);
+app.get('/api/2.0/angel/startups', angellistApis.getStartups);
+app.get('/api/2.0/angel/startup', angellistApis.getStartup);
+app.get('/api/2.0/profile', auth.ensureAuthenticated, userApis.getProfile); // must ensureAuth to send userid to getProfile
+app.get('/api/2.0/profile/getkey', auth.ensureAuthenticated, auth.createAPIToken);
+app.post('/api/2.0/invite', auth.ensureAuthenticated, auth.inviteUser);
+app.put('/api/2.0/profile/role', auth.ensureAuthenticated, userApis.setRole);
+app.post('/api/2.0/profile/remove/:userid', auth.ensureAuthenticated, userApis.removeProfile);
+app.post('/api/2.0/feedback', auth.ensureAuthenticated, userApis.feedback);
+
+// new for 2.0
+app.get('/api/2.0/companies', companyApis.companySearch);
+app.put('/api/2.0/settings', auth.ensureAuthenticated, communityApis.setCommunity);
+app.post('/api/2.0/contact', userApis.contactUser);
+
 app.get('/api/2.1/key/:key', communityApis.getKey);
 app.get('/api/2.1/users', userApis.userSearch);
 app.get('/api/2.1/search', userApis.directSearch);
