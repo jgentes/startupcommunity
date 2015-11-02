a4a8257ff5e908c447608e0f4b0232cd0e507179
James Gentes <james@startupcommunity.org>
2015-11-02 15:24:27 -0800

    quick fix

--- app/components/common/welcome/welcome.controller.js
+++ app/components/common/welcome/welcome.controller.js
@@ -13,7 +13,7 @@ function WelcomeController($auth, $q, $http, $mixpanel, $stateParams, $scope, $s
     this.community = community; // used in add company (not welcome) modal
     this.parents = [ 'Agriculture', 'Art', 'Construction', 'Consumer Goods', 'Corporate', 'Education', 'Finance', 'Government', 'Healthcare', 'Legal', 'Manufacturing', 'Medical', 'Non-Profit', 'Recreation', 'Services', 'Tech', 'Transportation' ];
     this.stages = [ 'Bootstrap', 'Seed', 'Series A', 'Series B', 'Later'];
-    this.user = user.data.user ? user.data.user : user;
+    this.user = user.data && user.data.user ? user.data.user : user;
 
     this.shouldIadd = function() {
         self.alert = {type: "warning", message: "Startups find experts based on their work experience. If you work with the company while living in the community, you should add it."}
