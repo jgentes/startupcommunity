dab684256db83aaade249af90c65dd82d4c57ba8
James Gentes <james@startupcommunity.org>
2015-11-24 13:35:46 -0800

    no posting

--- app/components/common/email.controller.js
+++ app/components/common/email.controller.js
@@ -16,17 +16,43 @@ function EmailController($http, $httpParamSerializer) {
 */
         $http.post('http://email.startupcommunity.org/includes/app/create.php', {
                 params: {
-                    'app_name': "push_brand_test",
-                    'from_name': "James Zibtru",
-                    'from_email': "james@bendtech.com",
-                    'reply_to': "james@bendtech.com"
+                    app_name: "push_brand_test",
+                    from_name: "James Zibtru",
+                    from_email: "james@bendtech.com",
+                    reply_to: "james@bendtech.com"
                 },
+                transformRequest: angular.identity,
                 headers: {
-                    "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+                    "Content-Type": undefined
                 }
             })
             .then(function(response) {
                 console.log(response);
             })
     }
+
+    this.login = function() {
+        /*
+         $httpParamSerializer({
+         'app_name': "push_brand_test",
+         'from_name': "James Zibtru",
+         'from_email': "james@bendtech.com",
+         'reply_to': "james@bendtech.com"
+         });
+         */
+        $http.post('http://email.startupcommunity.org/includes/login/main.php', {
+                params: {
+                    email: "jgentes@gmail.com",
+                    password: "3OreHw2Z",
+                    redirect: ""
+                },
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+                }
+            })
+            .then(function(response) {
+                console.log(response);
+            })
+    }
+
 }
