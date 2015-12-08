85774deb07eadda7415a981e071f195b41597e94
James Gentes <james@thesocialbusiness.com>
2015-12-07 21:19:42 -0800

    update for progress bar

--- app/components/common/dashboard/dashboard.controller.js
+++ app/components/common/dashboard/dashboard.controller.js
@@ -7,6 +7,8 @@ function DashboardController($stateParams, top) {
     var self = this;
     if (top && top.data) this.top = top.data;
 
+    this.max = this.top.parents[0].value + this.top.parents[1].value + this.top.parents[2].value;
+
     this.location_path = $stateParams.location_path;
 
     angular.element(document).ready(function () {
