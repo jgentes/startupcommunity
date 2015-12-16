433ac08a70dd909f13104cccb9c797572c36c765
James Gentes <james@startupcommunity.org>
2015-12-16 13:56:24 -0800

    fixed npm

--- app/components/common/nav/nav.controller.js
+++ app/components/common/nav/nav.controller.js
@@ -372,7 +372,7 @@ function NavigationController($auth, $state, $window, $timeout, $location, $scop
             var storage = JSON.parse($window.localStorage.getItem('startupcommunity-embed'))[domain];
         }
 
-        if (storage) {npm
+        if (storage) {
             this.color = storage.color;
             if (storage.full) this.embedded = false;
         }
