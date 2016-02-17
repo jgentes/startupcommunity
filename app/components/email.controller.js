f6a5425488fb87daba93e45453fcb66b7e65665c
James Gentes <james@startupcommunity.org>
2016-02-16 18:56:34 -0800

    email working finally

--- app/components/email.controller.js
+++ app/components/email.controller.js
@@ -2,8 +2,36 @@ angular
     .module('startupcommunity')
     .controller('EmailController', EmailController);
 
-function EmailController($http, $httpParamSerializer) {
+function EmailController($http, $httpParamSerializer, $sce, user) {
     var self = this;
+    console.log(user);
+
+    if (user) {
+
+        $http({
+            url: 'https://email.startupcommunity.org/includes/login/main.php',
+            method: 'POST',
+            data: $httpParamSerializer({
+                email: "jgentes@gmail.com",
+                password: "3OreHw2Z",
+                redirect: ""
+            }),
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then(function (response) {
+                self.working = false;
+                //var cookie = CryptoJS.SHA512("4" + "jgentes@gmail.com" + CryptoJS.SHA512("3OreHw2Z" + "PectGtma").toString() + "PectGtma").toString();
+                //$cookies.put('logged_in', cookie);
+
+                self.frame_content = $sce.trustAsHtml(response.data);
+
+            });
+
+    } else self.frame_content = "<p style='font-size: 24px;'>Please <a href='/login'>log in</a> to access this feature..</p>";
+
 
     this.createBrand = function() {
 /*
@@ -29,36 +57,6 @@ function EmailController($http, $httpParamSerializer) {
             .then(function(response) {
                 console.log(response);
             })
-    }
-
-    this.login = function() {
-        self.working = true;
-        /*
-         $httpParamSerializer({
-         'app_name': "push_brand_test",
-         'from_name': "James Zibtru",
-         'from_email': "james@bendtech.com",
-         'reply_to': "james@bendtech.com"
-         });
-         */
-
-        $http({
-            url: 'https://email.startupcommunity.org/includes/login/main.php',
-            method: 'POST',
-            data: $httpParamSerializer({
-                email: "jgentes@gmail.com",
-                password: "3OreHw2Z",
-                redirect: ""
-            }),
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        })
-        .then(function (response) {
-            self.working = false;
-            document.getElementById('framed_email').src = document.getElementById('framed_email').src;
-        })
-
     }
 
 }
