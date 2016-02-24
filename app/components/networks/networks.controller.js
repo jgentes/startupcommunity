2ef2b79ab4c14abdca804524840b86738a7ca66d
Andrey Miroshnichenko <a@avm.name>
2016-02-24 13:59:51 +0300

    Networks controller lists population

--- app/components/networks/networks.controller.js
+++ app/components/networks/networks.controller.js
@@ -2,6 +2,26 @@ angular
     .module('startupcommunity')
     .controller('NetworksController', NetworksController)
 
-function NetworksController($stateParams) {
-    this.pageTitle = $stateParams.pageTitle;
+function NetworksController($stateParams, location, nav_communities) {
+    this.networks = this.networks || {};
+    this.nav_communities = nav_communities;
+    this.location = location;
+    this.location_key = this.location.key;
+
+    for (item in this.nav_communities) {
+        var currentItem = this.nav_communities[item];
+        if (currentItem['type'] === 'network') {
+            var community = this.nav_communities[item];
+            if (community.community_profiles &&
+                community.community_profiles[this.location_key] &&
+                community.community_profiles[this.location_key].parents &&
+                community.community_profiles[this.location_key].parents[0]) {
+                var network_type = community.community_profiles[this.location_key].parents[0];
+                if (!this.networks[network_type]) this.networks[network_type] = {};
+                this.networks[network_type][item] = this.nav_communities[item];
+            }
+        }
+    }
+
+    console.log(this.networks);
 }
