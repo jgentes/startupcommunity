90136f38b9303561646c5e52f467a2dc822ce5fe
James Gentes <james@startupcommunity.org>
2016-02-26 08:09:42 -0800

    addedq uote

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -315,7 +315,8 @@ function randomQuote() {
         "&quot;The only way to do great work is to love what you do.&quot; -- Steve Jobs",
         "&quot;If I had an hour to solve a problem, I'd spend 55 minutes thinking about the problem and 5 minutes thinking about solutions.&quot; -- Alert Einstein",
         "&quot;The reasonable person adapts themselves to the world. The unreasonable one persists in trying to adapt the world to themselves. Therefore all progress depends on the unreasonable person.&quot; -- George Bernard Shaw",
-        "&quot;The truth is, the cost of preventing errors is often far greater than the cost of fixing them.&quot; -- Ed Catmull, President of Pixar"
+        "&quot;The truth is, the cost of preventing errors is often far greater than the cost of fixing them.&quot; -- Ed Catmull, President of Pixar",
+        "&quot;Believe in yourself. Under-confidence leads to a self-fulfilling prophecy that you are not good enough for your work.&quot; -- Dr. Roopleen"
 
     ];
     var rand = quotes[Math.floor(Math.random() * quotes.length)];
