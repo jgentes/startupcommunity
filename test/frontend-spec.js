8d3fc8a1906c19cb149b325d7537104e36cace05
Andrey Miroshnichenko <a@avm.name>
2016-02-28 21:56:25 +0300

    Split frontend and blog specs

--- test/frontend-spec.js
+++ test/frontend-spec.js
@@ -38,30 +38,3 @@ describe('Startup Community frontend page', function() {
     expect(element(by.css('footer > .sub-footer')).isPresent()).toBe(true);
   });
 });
-
-describe('Startup Community blog page', function() {
-  beforeEach(function() {
-    isAngularSite(false);
-  });
-
-  it('should open blog home', function() {
-    browser.get(browser.baseUrl + '/blog');
-
-    expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/blog/');
-  });
-
-  it('have header, posts and footer', function() {
-    // Header
-    expect(element(by.id('header')).isPresent()).toBe(true);
-
-    // Content area
-    expect(element(by.css('main.content')).isPresent()).toBe(true);
-
-    // Contains posts
-    expect(element(by.css('div.timeline article.post')).isPresent()).toBe(true);
-
-    // Footer
-    expect(element(by.id('footer')).isPresent()).toBe(true);
-  });
-});
-
