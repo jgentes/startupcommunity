3c93e6076c66242c3f8ceeadcb200b7287cb48ef
Andrey Miroshnichenko <a@avm.name>
2016-03-06 21:17:56 +0300

    Improved tests for user dashboard page

--- test/users/user.dashboard.spec.js
+++ test/users/user.dashboard.spec.js
@@ -41,14 +41,90 @@ describe('User:Dashboard', function () {
                     evaluate('profile.user.roles.founder')
         ).toBeTruthy();
 
+        // check if displayed number of companies on tab matches actual number of companies on profile
+        element.all( by.binding('profile.user') ).
+            first().
+                evaluate('profile.companies.founder').
+                    then(function(founder) {
+                        expect(
+                            element.all( by.exactBinding('profile.companies.count.founder') ).
+                                count()
+                        ).toEqual(Object.keys(founder).length);
+                    });
+
+        // button "Ask me..." is present
+        expect(
+            element( by.css('[ng-click^="profile.ask"]') ).
+                isPresent()
+        ).toBeTruthy();
+
+        // button "Contact me..." is present
+        expect(
+            element( by.css('[ng-click^="profile.contact"]') ).
+                isPresent()
+        ).toBeTruthy();
+
         // check rendered skill list agains number of skills in user.profile.skills
         element.all( by.binding('profile.user') ).
             first().
-                evaluate('profile.user.profile.skills').then(function(v) {
-                    expect(
-                        element.all( by.repeater('skill in profile.user.profile.skills') ).
-                            count()
-                    ).toEqual(v.length * 2);  // appears two times on page, so need to multiply checked value by 2
+                evaluate('profile.user.profile.skills').
+                    then(function(v) {
+                        expect(
+                            element.all( by.exactRepeater('skill in profile.user.profile.skills') ).
+                                count()
+                        ).toEqual(v.length * 2);  // appears two times on page, so need to multiply checked value by 2
+                    });
+    });
+
+    //================================================================================================================
+    // Ask a Question tab verification
+    //----------------------------------------------------------------------------------------------------------------
+    it('ask question tab has correct data', function () {
+        // check for presence of "Ask a Question" tab link
+        expect(
+            element( by.id('ask_li') ).
+                isPresent()
+        ).toBeTruthy();
+
+        // Try to click on "Ask a Question" tab link and check its class to become active
+        element( by.id('ask_li') ).
+            click().
+                then(function() {
+                    element( by.id('ask_li') ).
+                        getAttribute('class').
+                            then(function(classes) {
+                                var classNames = classes.split(' ');
+
+                                expect(
+                                    classNames.indexOf('active') >= 0
+                                ).toBeTruthy();
+                            });
                 });
+    });
+
+    //================================================================================================================
+    // Full Profile tab verification
+    //----------------------------------------------------------------------------------------------------------------
+    it('full profile tab has correct data', function () {
+        // check for presence of "Full Profile" tab link
+        expect(
+            element( by.id('profile_li') ).
+                isPresent()
+        ).toBeTruthy();
+
+        // Try to click on "Full Profile" tab link and check its class to become active
+        element( by.id('profile_li') ).
+            click().
+            then(function() {
+                element( by.id('profile_li') ).
+                    getAttribute('class').
+                    then(function(classes) {
+                        var classNames = classes.split(' ');
+
+                        expect(
+                            classNames.indexOf('active') >= 0
+                        ).toBeTruthy();
+                    });
+            });
     });
 });
