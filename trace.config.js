c938c15a82031b313011df831a13bf1cc317cce0
James Gentes <james@startupcommunity.org>
2016-03-09 13:18:50 -0800

    fixed rename operation

--- trace.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    serviceName: 'Community API',
-    apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU2ZTA0Y2U1NDBhODA3MTEwMDRiYjViYiIsImlhdCI6MTQ1NzU0MDMyNX0.mydutWKhZt_dQkOJClFHQ9VHQa6EpNeQ_rtCEaSYGd0'
-};
