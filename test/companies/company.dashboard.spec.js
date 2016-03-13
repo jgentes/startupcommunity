3ea937ca1a465452315ecf83d1b71e18c58e5181
Andrey Miroshnichenko <a@avm.name>
2016-03-13 21:43:17 +0300

    Company dashboard validation

---
+++ test/companies/company.dashboard.spec.js
@@ -0,0 +1,45 @@
+describe('Company:Dashboard', function () {
+    beforeEach(function () {
+        isAngularSite(true);
+
+        browser.get('/startupcommunity');
+    });
+
+    function companyRef() {
+        return element.all( by.binding('profile.company') ).
+            first().
+            evaluate('profile');
+    };
+
+    it('contains valid company details', function () {
+        companyRef().
+            then(function(company) {
+                // check community URL key
+                expect(
+                    company.community.key
+                ).toBe('startupcommunity');
+
+                // check community title
+                expect(
+                    company.profile.home
+                ).toBe('bend-or');
+
+                // check community type
+                expect(
+                    company.community.type
+                ).toBe('company');
+
+                // check number of role filters in sidebar
+                //expect(
+                //    element.all( by.css('[ng-click^="users.filterRole"]') ).
+                //        count()
+                //).toBe( 7 );
+
+                // verify number of rendered users boxes in list
+                //expect(
+                //    element.all( by.exactRepeater('item in users.users.results') ).
+                //        count()
+                //).toBe(users.users.count);
+            });
+    });
+});
