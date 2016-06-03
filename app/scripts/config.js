194a299013da221161c040438ff36ddae130ddb5
James Gentes <james@startupcommunity.org>
2016-06-03 13:51:32 -0700

    fixed bugsnag

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -337,7 +337,7 @@ angular
 
                 $log.error.apply( $log, arguments );
 
-                Bugsnag.notifyException(exception, {diagnostics:{cause: cause}});
+                if ($window.Bugsnag) $window.Bugsnag.notifyException(exception, {diagnostics:{cause: cause}});
 
                 try {
                     var errorMessage = exception.toString();
