a4dfde81ae70b96aada19c999a88f56db28486cd
James Gentes <j@tend.ai>
2018-04-29 16:17:55 +0000

    moved favicon

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              ['@babel/preset-env', {
+                shippedProposals: true,
+                targets: {
+                  browsers: ['>2%']
+                }
+              }]
+            ]
           }
         }
       },
