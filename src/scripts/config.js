e74d2f1394c54ef1b25d68f7c3c6ca2f8b5329dd
James Gentes <j@tend.ai>
2019-07-08 07:41:15 -0400

    change for window.open

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -317,7 +317,6 @@ angular
         $authProvider.loginRedirect = false; //otherwise will go home
         $authProvider.linkedin({
             clientId: '75bqixdv58z1az',
-            response_type: 'code',
             authorizationEndpoint: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&scope=r_liteprofile%20r_emailaddress',
             state: function() {
                 return window.location.pathname;
