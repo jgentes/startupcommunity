5035a1706e9bee632503867872f9627fb67d5685
James Gentes <j@tend.ai>
2019-07-13 16:16:19 -0700

    working!

--- src/components/auth/auth.controller.js
+++ src/components/auth/auth.controller.js
@@ -22,6 +22,8 @@ function LoginController($auth, $scope, $state, $stateParams, auth_service, swee
         return $window.open(urlString, '_blank','height=600,width=600');
     };
 
+    this.login = () => $location.url('/logout');
+
     this.clickToTweet = function() {
         var getQuote = document.getElementById('quote').innerHTML;
 
