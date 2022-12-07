9b0a2024b37d7f685997c528b6ea25f00502d39c
James Gentes <jgentes@users.noreply.github.com>
2022-12-07 12:46:35 -0800

    Update config.js

--- db/config/config.js
+++ db/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "development": {
-    "url": "mysql://bxaqqctwcaidtk79:j5b2s66qg9q3mo3l@dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n24fuljt9b5jqowy"
+    "url": ""
   },
   "staging": {
     "use_env_variable": "JAWSDB_URL"
